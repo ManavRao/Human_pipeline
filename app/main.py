02171e6ef9394e0a7fa3d3c1d01c654d79428116
ManavRao <rao.manav20@gmail.com>
2018-07-10 15:57:34 +0900

    adding codes

--- app/main.py
+++ app/main.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template
 import glob
 import random
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
+
 clusters = []
 
-
 def load_clusters():
-    temp1 = glob.glob("static\img\clusters\*")
-    # print(temp1)
+    add1 = ROOT_DIR + "/static/img/clusters/*"
+    temp1 = glob.glob(add1)
+    print(temp1)
     temp2 = []
 
     for i in temp1:
         i = i + "\*.jpg"
         temp2.append(i.replace("\\", "/"))
-    # print(temp2)
+    print(temp2)
     temp3 = []
 
     for i in temp2:
         temp3.append(glob.glob(i))
-    # print(temp3)
+    print(temp3)
 
     for i in temp3:
         lol = []
@@ -27,9 +31,9 @@ def load_clusters():
             lol.append(j.replace("\\", "/"))
         clusters.append(lol)
 
-
 load_clusters()
 
+numbers = random.sample(range(0, len(clusters)-1), 2)
 
 @app.route('/')
 @app.route('/index.html')
@@ -37,16 +41,19 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/pipeline1.html')
-def pipeline1():
-    num = random.randint(0, len(clusters)-1)
-    return render_template("pipeline1.html", temp=clusters, temp_len=len(clusters), temp_num=num)
+@app.route('/pipeline11.html')
+def pipeline11():
+    num = numbers[0]
+    return render_template("pipeline1.html", temp=clusters, temp_len=len(clusters), temp_num=num, flag_imp=0)
+
+@app.route('/pipeline12.html')
+def pipeline12():
+    num = numbers[1]
+    return render_template("pipeline1.html", temp=clusters, temp_len=len(clusters), temp_num=num, flag_imp=1)
 
 
 @app.route('/processing.html')
 def processing():
-    js = open("static\\js\\basic.js", "r")
-    print(js.read())
     return render_template("processing.html")
 
 
@@ -60,4 +67,4 @@ def pipeline2():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', threaded=True)
